cdfa2441b43ab40103a48eb3e5d123c8b4a30035
Amro6779 <mr487680@gmail.com>
2026-09-23 15:22:12 +0300

    finishing error page design

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//////////////////? elements //////////////////////
+
+const searchForm = document.querySelector("#search-form");
+const searchBtn = document.querySelector(".search-button");
+const searchInput = document.querySelector("#search-input");
+
+
+
+//////////////////! events ////////////////////////
+
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+
+//////////////////* functions /////////////////////// 
